7484a3ea9ae99204804bee00af2f2645eb02d2bb
xybuild <timileyindaso@gmail.com>
2024-08-30 13:30:14 +0100

    ap

--- infra/template/ec2_install_apache.sh
+++ infra/template/ec2_install_apache.sh
@@ -11,3 +11,11 @@ pip3 install -r requirements.txt
 echo 'Waiting for 30 seconds before running the app.py'
 setsid python3 -u app.py &
 sleep 30
+
+# install aws cli
+
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip" 
+
+unzip awscliv2.zip
+
+sudo ./aws/install --update
